81b3861cc9193f36cee52f33be439cb886f0644c
Víctor Monsalve <victor.monsalve@uv.cl>
2025-03-30 16:47:11 -0300

    bcrypt seed password users

--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -4,6 +4,7 @@ import { initialData } from './data/seed-data';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/auth/entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class SeedService {
@@ -31,6 +32,7 @@ export class SeedService {
     const users: User[] = [];
 
     seedUsers.forEach(user => {
+      user.password = bcrypt.hashSync(user.password, 10);
       users.push(this.userRepository.create(user));
     });
   
