697b7c66bc3daef94079014c903428092ded60f1
Víctor Monsalve <victor.monsalve@uv.cl>
2025-03-30 18:14:23 -0300

    documentacion DTOs

--- 04-teslo-shop/src/common/dtos/pagination.dto.ts
+++ 04-teslo-shop/src/common/dtos/pagination.dto.ts
@@ -1,14 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsInt, IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
 
+    @ApiProperty({
+        default: 10,
+        description: 'Cuántos registros necesitas'
+    })
     @IsOptional()
     @IsInt()
     @IsPositive()
     @Type(() => Number) // alternativa usar un validation pipe en el main.ts, es equivalente a enableImplicitConversions: true
     limit?: number
 
+    @ApiProperty({
+        default: 0,
+        description: 'Cuántos registros quieres saltar'
+    })
     @IsOptional()
     @IsInt()
     @Min(0)
--- 04-teslo-shop/src/products/dto/create-product.dto.ts
+++ 04-teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IS_ARRAY,
   IsArray,
@@ -11,10 +12,17 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+
+  @ApiProperty({
+    nullable: false,
+    description: 'Product title (unique)',
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   @IsOptional()
@@ -24,27 +32,33 @@ export class CreateProductDto {
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
